64dc11e5790e855b293c32416f7cc219d6156dbf
Max zhang <373884384@qq.com>
2021-04-21 16:15:19 +0800

    更新线报参数

--- src/Request/Feature/ListTipOffRequest.php
+++ src/Request/Feature/ListTipOffRequest.php
@@ -22,19 +22,19 @@ class ListTipOffRequest extends DataokeRequest
     /**
      * @var int 线报类型：1-超值买返2-天猫超市3-整点抢购4-最新线报-所有数据(默认)5-最新线报-天猫6-最新线报-京东7-最新线报-拼多多
      */
-    private $topic;
+    public $topic;
     /**
      * @var int rush-整点抢购时的时间戳（秒），示例：1617026400
      */
-    private $selectTime;
+    public $selectTime;
     /**
      * @var int 每页记录数，默认20
      */
-    private $pageId;
+    public $pageId;
     /**
      * @var int 页码，默认为1
      */
-    private $pageSize;
+    public $pageSize;
 
     function generParams()
     {
